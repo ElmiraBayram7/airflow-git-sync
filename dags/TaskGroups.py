66f8a1d25db4286cec418874b9f43776604ace57
ElmiraBayram7 <xudiyeva.elmira7@gmail.com>
2024-12-15 13:35:36 +0400

    added

--- dags/TaskGroups.py
+++ dags/TaskGroups.py
@@ -8,7 +8,7 @@ with DAG('group_dag', start_date=datetime(2022, 1, 1),
 
     args = {'start_date': dag.start_date, 'schedule_interval': dag.schedule_interval, 'catchup': dag.catchup}
 
-    # `download_tasks` fonksiyonunu çağırıyoruz
+  
     downloads = download_tasks()
 
     # `check_files` görevi
