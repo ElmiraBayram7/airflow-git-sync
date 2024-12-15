6c7cddcb2526da2b5727b921e0ba61856bb410f9
ElmiraBayram7 <xudiyeva.elmira7@gmail.com>
2024-12-15 16:00:26 +0400

    added

---
+++ dags/TaskGroupsWithHomework.py
@@ -0,0 +1,59 @@
+import sys
+sys.path.append('/opt/airflow/code_base/airflow-git-sync/dags/groups')
+
+from group_downloads_homework import download_tasks
+from airflow import DAG
+from datetime import datetime, timedelta
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.operators.python import PythonOperator
+import pandas as pd
+from sqlalchemy import create_engine
+import logging
+
+# Başlangıç tarihi ve veritabanı bağlantısı
+start_date = datetime(2024, 1, 1)
+engine = create_engine('postgresql://train:Ankara06@postgres:5432/traindb')
+
+# DAG için default_args parametreleri
+default_args = {
+    'owner': 'airflow',
+    'start_date': start_date,
+    'retries': 1,
+    'retry_delay': timedelta(seconds=5)
+}
+
+# DAG'ın tanımlanması
+with DAG(dag_id='airflow_hw1_dag', default_args=default_args, schedule_interval='@hourly', catchup=False) as dag:
+
+    # Schema oluşturma görevleri
+    t1 = PostgresOperator(
+        task_id='create_staging_schema', 
+        postgres_conn_id='postgresql_conn',
+        sql="CREATE SCHEMA IF NOT EXISTS staging AUTHORIZATION train;"
+    )
+
+    # Verileri indiren görevleri çağıran task group
+    downloads = download_tasks()
+
+    # Yeni schema oluşturma görevleri
+    t5 = PostgresOperator(
+        task_id='create_serving_schema', 
+        postgres_conn_id='postgresql_conn',
+        sql="CREATE SCHEMA IF NOT EXISTS serving AUTHORIZATION train;"
+    )
+
+    # `v_product_status_track` view'inin oluşturulması
+    t6 = PostgresOperator(
+        task_id='create_v_product_status_track', 
+        postgres_conn_id='postgresql_conn',
+        sql="""
+        CREATE OR REPLACE VIEW serving.v_product_status_track AS
+        SELECT * 
+        FROM staging.order_items oi
+        JOIN staging.orders o ON oi."orderItemOrderId" = o."orderId"
+        JOIN staging.products p ON oi."orderItemProductId" = p."productId"
+        """
+    )
+
+    # Görevlerin sıralanması: önce schema, sonra veri indirme ve sonrasında view oluşturma
+    t1 >> downloads >> t5 >> t6
---
+++ dags/groups/group_downloads_homework.py
@@ -0,0 +1,48 @@
+from airflow import DAG
+from datetime import datetime, timedelta
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.operators.python import PythonOperator
+import pandas as pd
+from sqlalchemy import create_engine
+from airflow.utils.task_group import TaskGroup
+
+# PostgreSQL bağlantısı
+engine = create_engine('postgresql://train:Ankara06@postgres:5432/traindb')
+
+# CSV'yi PostgreSQL'e yükleyen fonksiyon
+def from_github_to_postgresql_staging(**kwargs):
+    base_url = kwargs['base_url']
+    schema = kwargs['schema']
+    table_name = kwargs['table_name']
+    engine = kwargs['engine']    
+    df = pd.read_csv(base_url)
+    # Veriyi PostgreSQL'e yazma
+    df.to_sql(name=table_name, con=engine, schema=schema, if_exists='replace', index=False)
+
+
+
+# İndirme görevlerini içeren TaskGroup fonksiyonu
+def download_tasks():
+    with TaskGroup("downloads", tooltip="Download tasks") as group:
+        t1 = PythonOperator(
+            task_id='from_github_to_postgresql_staging_orders', 
+            python_callable=from_github_to_postgresql_staging,
+            op_kwargs={'base_url': "https://raw.githubusercontent.com/erkansirin78/datasets/master/retail_db/orders.csv",
+                       'table_name': 'orders', 'schema': 'staging', 'engine': engine}
+        )
+
+        t2 = PythonOperator(
+            task_id='from_github_to_postgresql_staging_order_items', 
+            python_callable=from_github_to_postgresql_staging,
+            op_kwargs={'base_url': "https://raw.githubusercontent.com/erkansirin78/datasets/master/retail_db/order_items.csv",
+                       'table_name': 'order_items', 'schema': 'staging', 'engine': engine}
+        )
+
+        t3 = PythonOperator(
+            task_id='from_github_to_postgresql_staging_products', 
+            python_callable=from_github_to_postgresql_staging,
+            op_kwargs={'base_url': "https://raw.githubusercontent.com/erkansirin78/datasets/master/retail_db/products.csv",
+                       'table_name': 'products', 'schema': 'staging', 'engine': engine}
+        )
+
+        return group
