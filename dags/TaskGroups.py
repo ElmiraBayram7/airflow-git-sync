c98c1a5b75d20fec95ddfb9809ba8b903521d624
ElmiraBayram7 <xudiyeva.elmira7@gmail.com>
2024-12-15 13:36:42 +0400

    added

--- dags/TaskGroups.py
+++ dags/TaskGroups.py
@@ -3,7 +3,7 @@ from airflow.operators.bash import BashOperator
 from groups.group_downloads import download_tasks
 from datetime import datetime
 
-with DAG('group_dag', start_date=datetime(2022, 1, 1),
+with DAG('group_dag', start_date=datetime(2024, 1, 1),
          schedule_interval='@daily', catchup=False) as dag:
 
     args = {'start_date': dag.start_date, 'schedule_interval': dag.schedule_interval, 'catchup': dag.catchup}
