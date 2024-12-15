cbc5429eecab38b87b8c67f227afd192f35e70b4
ElmiraBayram7 <xudiyeva.elmira7@gmail.com>
2024-12-15 13:11:36 +0400

    added

--- dags/TaskGroups.py
+++ dags/TaskGroups.py
@@ -14,11 +14,16 @@ default_args = {
 
 with DAG('my_dag', default_args=default_args, schedule_interval='@daily', catchup=False) as dag:
 
-    downloads=download_tasks
+    # `download_tasks` fonksiyonunu çağırıyoruz
+    downloads = download_tasks()
 
-    check_files=BashOperator(task_id='check_file_exists', bash_command='sha256sum /tmp/dirty_store_transactions.csv /tmp/Churn_Modelling.csv',
-                      retries=2, retry_delay=timedelta(seconds=15))
+    # Tek bir BashOperator ile iki dosyanın checksum kontrolü yapılacak
+    check_files = BashOperator(
+        task_id='check_file_exists', 
+        bash_command='sha256sum /tmp/dirty_store_transactions.csv /tmp/Churn_Modelling.csv',
+        retries=2, 
+        retry_delay=timedelta(seconds=15)
+    )
 
-    
-    
-    downloads>>check_files
+    # `downloads` içindeki task'lerden sonra `check_files` çalışacak
+    downloads >> check_files
