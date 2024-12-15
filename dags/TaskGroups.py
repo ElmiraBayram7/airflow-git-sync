b7db8a2558774d03c5384ffa7674ccab0a604e4e
ElmiraBayram7 <xudiyeva.elmira7@gmail.com>
2024-12-15 14:12:23 +0400

    added

--- dags/TaskGroups.py
+++ dags/TaskGroups.py
@@ -6,7 +6,7 @@ from datetime import datetime
 with DAG('group_dag', start_date=datetime(2022, 1, 1),
          schedule_interval='@daily', catchup=False) as dag:
 
-    #args = {'start_date': dag.start_date, 'schedule_interval': dag.schedule_interval, 'catchup': dag.catchup}
+    args = {'start_date': dag.start_date, 'schedule_interval': dag.schedule_interval, 'catchup': dag.catchup}
 
     ## `download_tasks` fonksiyonunu çağırıyoruz
     downloads = download_tasks()
